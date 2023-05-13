6cd0d1be947155f188fdcb1abd86bbdfebc47e67
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-05-13 18:37:34 +0600

    basic express, html, css file load in server.js

--- Express_JS/Express_Basic_Server/server.js
+++ Express_JS/Express_Basic_Server/server.js
@@ -1,8 +1,30 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.all("/", (req,res)=>{
-    res.send("<h1>Hello Express JS</h1>");
+app.use(express.static('public'));
+
+app.get("/json", (req,res)=>{
+    res.json({
+        "name": "Sabbir",
+        "age": 23,
+        "dept": "ECE"
+    });
+})
+app.get("/user", (req,res)=>{
+    res.sendFile(path.join(__dirname + "/public/index.html"))
+})
+app.post("/", (req,res)=>{
+    res.send("<h1>Post Method</h1>");
+})
+app.put("/", (req,res)=>{
+    res.send("<h1>Put Method</h1>");
+})
+app.patch("/", (req,res)=>{
+    res.send("<h1>Patch Method</h1>");
+})
+app.delete("/", (req,res)=>{
+    res.send("<h1>Delete Method</h1>");
 })
 
 
