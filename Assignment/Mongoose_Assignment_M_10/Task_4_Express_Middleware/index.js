65ecd99725377bbe35922a18453b43dc599a5bf9
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-06-08 22:49:21 +0600

    Assignemnt 10 Added

---
+++ Assignment/Mongoose_Assignment_M_10/Task_4_Express_Middleware/index.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+function authenticate(req, res, next) {
+  const token = req.headers.authorization;
+  
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+}
+
+module.exports = authenticate;
